f54afb9cfff75f05e87b3adf63f0b5314d9db102
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-06-05 16:17:01 -0500

    subiendo mejoras

--- frontend/src/app/components/brand-experience/brand-experience.component.ts
+++ frontend/src/app/components/brand-experience/brand-experience.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-brand-experience',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './brand-experience.component.html',
   styleUrl: './brand-experience.component.css'
 })
